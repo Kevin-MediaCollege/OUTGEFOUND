9c0133fec16336a9623d4bf30a79bf013eb57303
Kevin Krol <Krol.kevin@gmail.com>
2016-02-08 09:27:49 +0100

    Added an enity

---
+++ Assets/Scripts/Entity/Entity.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Entity : MonoBehaviour
+{
+	public static IEnumerable<Entity> All
+	{
+		get
+		{
+			return all;
+		}
+	}
+
+	private static HashSet<Entity> all = new HashSet<Entity>();
+
+	[SerializeField] private string[] startingTags;
+
+	private HashSet<string> tags;
+
+	#region Unity Callbacks
+	protected void Awake()
+	{
+		tags = new HashSet<string>();
+
+		// Add the starting tags
+		foreach(string tag in startingTags)
+		{
+			tags.Add(tag);
+		}
+	}
+
+	protected void OnEnable()
+	{
+		all.Add(this);
+	}
+
+	protected void OnDisable()
+	{
+		all.Remove(this);
+	}
+	#endregion
+
+	public void AddTag(string tag)
+	{
+		tags.Add(tag);
+	}
+
+	public void RemoveTag(string tag)
+	{
+		tags.Remove(tag);
+	}
+
+	public bool HasTag(string tag)
+	{
+		return tags.Contains(tag);
+	}
+}
---
+++ Assets/Scripts/Entity/EntityUtils.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class EntityUtils
+{
+	public static IEnumerable<Entity> GetEntitiesWithTag(string tag)
+	{
+		HashSet<Entity> result = new HashSet<Entity>();
+
+		foreach(Entity entity in Entity.All)
+		{
+			if(entity.HasTag(tag))
+			{
+				result.Add(entity);
+			}
+		}
+
+		return result;
+	}
+
+	public static Entity GetEntityWithTag(string tag)
+	{
+		foreach(Entity entity in Entity.All)
+		{
+			if(entity.HasTag(tag))
+			{
+				return entity;
+			}
+		}
+
+		return null;
+	}
+}
