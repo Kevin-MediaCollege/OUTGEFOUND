e6bde255212202257800146401b823222bde546a
Ted-Laptop-New <meatkroket@hotmail.com>
2016-02-22 13:08:33 +0100

    Added cover

--- Assets/Scripts/AI/Cover/CoverBase.cs
+++ Assets/Scripts/AI/Cover/CoverBase.cs
@@ -3,19 +3,35 @@ using System.Collections;
 
 public class CoverBase : MonoBehaviour 
 {
-	public Vector2 coverSize;
+	[Range(0.2f, 3f)]
+	public float coverSizeX;
+
+	[Range(0.2f, 3f)]
+	public float coverSizeY;
+
+	[Range(0.3f, 1.5f)]
 	public float coverOffset;
+
+	void Awake()
+	{
+		
+	}
 
 	void OnDrawGizmosSelected()
 	{
-		Vector3 left = gameObject.transform.position + (gameObject.transform.forward * coverOffset) + (gameObject.transform.right * coverSize.x);
-		Vector3 right = gameObject.transform.position + (gameObject.transform.forward * coverOffset) + (-(gameObject.transform.right * coverSize.x));
-		Vector3 height = new Vector3(0f, coverSize.y, 0f);
+		Vector3 left = gameObject.transform.position + (gameObject.transform.forward * coverOffset) + (gameObject.transform.right * coverSizeX);
+		Vector3 right = gameObject.transform.position + (gameObject.transform.forward * coverOffset) + (-(gameObject.transform.right * coverSizeX));
+		Vector3 height = new Vector3(0f, coverSizeY, 0f);
 
 		Gizmos.color = new Color(1f, 0f, 0f);
 		Gizmos.DrawLine(left, right);
 		Gizmos.DrawLine(left, left + height);
 		Gizmos.DrawLine(right, right + height);
 		Gizmos.DrawLine(left + height, right + height);
+	}
+
+	public float getAngle()
+	{
+		return gameObject.transform.eulerAngles.y;
 	}
 }
---
+++ Assets/Scripts/AI/Cover/CoverManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CoverManager : MonoBehaviour 
+{
+	public static CoverManager instance;
+	public List<CoverBase> coverList;
+
+	public Transform a;
+
+	public CoverBase coverTest;
+
+	void Awake () 
+	{
+		instance = this;
+	}
+
+	void Update()
+	{
+		Debug.Log ("cover=" + isCoverSafe (coverTest, a.position));
+	}
+
+	public void addCover(CoverBase _cover)
+	{
+		coverList.Add (_cover);
+	}
+
+	public void getClosestCover()
+	{
+		Vector3 playerPos = EntityUtils.GetEntityWithTag ("Player").gameObject.transform.position;
+	}
+
+	public bool isCoverSafe(CoverBase _cover, Vector3 _playerPos)
+	{
+		float r = RotationHelper.rotationToPoint (_cover.gameObject.transform.position, _playerPos);
+		return Mathf.Abs((_cover.getAngle() - RotationHelper.fixRotation(_cover.getAngle(), r))) < 60f ? true : false;
+	}
+}
