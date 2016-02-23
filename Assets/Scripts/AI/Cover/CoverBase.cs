7ba2d386579f2e4e569630b017dc30f32ae2f758
Ted-Laptop-New <meatkroket@hotmail.com>
2016-02-23 15:35:17 +0100

    Finished CoverManager

--- Assets/Scripts/AI/Cover/CoverBase.cs
+++ Assets/Scripts/AI/Cover/CoverBase.cs
@@ -3,18 +3,33 @@ using System.Collections;
 
 public class CoverBase : MonoBehaviour 
 {
+	[Header("Height of the cover.")]
 	[Range(0.2f, 3f)]
 	public float coverSizeX;
 
+	[Header("Width of the cover.")]
 	[Range(0.2f, 3f)]
 	public float coverSizeY;
 
+	[Header("Distance from player position to cover")]
 	[Range(0.3f, 1.5f)]
 	public float coverOffset;
+
+	[Header("Safe to take cover angle")]
+	[Range(10f, 90f)]
+	public float coverAngle;
+
+	[HideInInspector]
+	public bool isSafe;
 
 	void Awake()
 	{
 		
+	}
+
+	void Start()
+	{
+		CoverManager.instance.addCover (this);
 	}
 
 	void OnDrawGizmosSelected()
@@ -28,6 +43,10 @@ public class CoverBase : MonoBehaviour
 		Gizmos.DrawLine(left, left + height);
 		Gizmos.DrawLine(right, right + height);
 		Gizmos.DrawLine(left + height, right + height);
+
+		Gizmos.color = new Color(0f, 0f, 1f);
+		Gizmos.DrawLine(gameObject.transform.position, gameObject.transform.position + new Vector3(Mathf.Sin((gameObject.transform.eulerAngles.y + coverAngle) * Mathf.PI / 180f) * 5f, 0f, Mathf.Cos((gameObject.transform.eulerAngles.y + coverAngle) * Mathf.PI / 180f) * 5f));
+		Gizmos.DrawLine(gameObject.transform.position, gameObject.transform.position + new Vector3(Mathf.Sin((gameObject.transform.eulerAngles.y - coverAngle) * Mathf.PI / 180f) * 5f, 0f, Mathf.Cos((gameObject.transform.eulerAngles.y - coverAngle) * Mathf.PI / 180f) * 5f));
 	}
 
 	public float getAngle()
--- Assets/Scripts/AI/Cover/CoverManager.cs
+++ Assets/Scripts/AI/Cover/CoverManager.cs
@@ -5,35 +5,69 @@ using System.Collections.Generic;
 public class CoverManager : MonoBehaviour 
 {
 	public static CoverManager instance;
-	public List<CoverBase> coverList;
+	private List<CoverBase> coverList;
+	private int coverListLength;
+	private bool coverUpdated;
 
 	public Transform a;
-
-	public CoverBase coverTest;
+	public Transform b;
+	public Transform c;
+	public NavMeshAgent agentA;
+	public NavMeshAgent agentB;
 
 	void Awake () 
 	{
 		instance = this;
+		coverList = new List<CoverBase>();
 	}
 
 	void Update()
 	{
-		Debug.Log ("cover=" + isCoverSafe (coverTest, a.position));
+		coverUpdated = false;
+
+		CoverBase cb = getClosestCover (b.position);
+		if(cb != null)
+		{
+			c.transform.position = cb.gameObject.transform.position;
+			agentA.SetDestination (b.transform.position);
+			agentB.SetDestination (c.transform.position);
+		}
 	}
 
 	public void addCover(CoverBase _cover)
 	{
 		coverList.Add (_cover);
+		coverListLength++;
 	}
 
-	public void getClosestCover()
+	public CoverBase getClosestCover(Vector3 _current)
 	{
-		Vector3 playerPos = EntityUtils.GetEntityWithTag ("Player").gameObject.transform.position;
+		CoverBase cover = null;
+		float closest = 999999f;
+
+		Vector3 playerPos = a.position;//EntityUtils.GetEntityWithTag ("Player").gameObject.transform.position;
+		for (int i = 0; i < coverListLength; i++)
+		{
+			if ((!coverUpdated && isCoverSafe (coverList[i], playerPos)) || (coverUpdated && coverList[i].isSafe)) 
+			{
+				float dist = Vector3.Distance (_current, coverList [i].gameObject.transform.position);
+				if(dist < closest)
+				{
+					closest = dist;
+					cover = coverList [i];
+				}
+			}
+		}
+
+		return cover;
 	}
 
 	public bool isCoverSafe(CoverBase _cover, Vector3 _playerPos)
 	{
-		float r = RotationHelper.rotationToPoint (_cover.gameObject.transform.position, _playerPos);
-		return Mathf.Abs((_cover.getAngle() - RotationHelper.fixRotation(_cover.getAngle(), r))) < 60f ? true : false;
+		bool result = Mathf.Abs ((_cover.getAngle () - RotationHelper.fixRotation (_cover.getAngle (), 
+			RotationHelper.rotationToPoint (_cover.gameObject.transform.position, _playerPos)))) < _cover.coverAngle ? true : false;
+		
+		_cover.isSafe = result;
+		return result;
 	}
 }
