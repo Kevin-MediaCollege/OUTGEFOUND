b501c6cd20e0fea4c2ec274022a2270b1da4a253
Kevin Krol <Krol.kevin@gmail.com>
2016-02-26 14:38:12 +0100

    WIP doors

---
+++ Assets/Scripts/Misc/Door.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Door : MonoBehaviour
+{
+	[SerializeField] private Animator animator;
+	[SerializeField] private float range;
+
+	protected void FixedUpdate()
+	{
+		bool open = false;
+
+		foreach(Entity entity in Entity.All)
+		{
+			float distance2 = (transform.position - entity.transform.position).sqrMagnitude;
+
+			if(distance2 < range * range)
+			{
+				open = true;
+				break;
+			}
+		}
+
+		animator.SetBool("Open", open);
+	}
+
+	protected void OnDrawGizmos()
+	{
+		Gizmos.color = Color.cyan;
+		Gizmos.DrawWireSphere(transform.position, range);
+	}
+}
