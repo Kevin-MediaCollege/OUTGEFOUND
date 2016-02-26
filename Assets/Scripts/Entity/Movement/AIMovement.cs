14aa769bd73baca46363401542c69ceaaa801b9b
Kevin Krol <Krol.kevin@gmail.com>
2016-02-26 10:03:07 +0100

    Added AI movement script

---
+++ Assets/Scripts/Entity/Movement/AIMovement.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AIMovement : EntityMovement
+{
+	[SerializeField] private NavMeshAgent navMeshAgent;
+
+	public bool MoveTo(Vector3 point)
+	{
+		if(navMeshAgent.SetDestination(point))
+		{
+			navMeshAgent.speed = speed;
+			return true;
+		}
+
+		return false;
+	}
+}
