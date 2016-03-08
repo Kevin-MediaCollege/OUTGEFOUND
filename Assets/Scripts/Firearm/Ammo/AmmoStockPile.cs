83d5361656b17d8dd5e5c6f6deecb3a1a701d369
Kevin Krol <Krol.kevin@gmail.com>
2016-03-08 17:46:59 +0100

    Fixed buying ammo not working

--- Assets/Scripts/Firearm/Ammo/AmmoStockPile.cs
+++ Assets/Scripts/Firearm/Ammo/AmmoStockPile.cs
@@ -25,10 +25,27 @@ public class AmmoStockPile : MonoBehaviour
 	[SerializeField] private int start;
 	[SerializeField] private int capacity;
 
+	private Entity entity;
 	private int current;
 
 	protected void Awake()
 	{
+		entity = GetComponent<Entity>() ?? GetComponentInParent<Entity>();
 		current = start;
+	}
+
+	protected void OnEnable()
+	{
+		entity.Events.AddListener<RefillAmmoEvent>(OnRefillAmmoEvent);
+	}
+
+	protected void OnDisable()
+	{
+		entity.Events.RemoveListener<RefillAmmoEvent>(OnRefillAmmoEvent);
+	}
+
+	private void OnRefillAmmoEvent(RefillAmmoEvent evt)
+	{
+		current = capacity;
 	}
 }
