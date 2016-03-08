7128ba835cbcbe1addc8047c0ab53bc47cfcad97
Kevin Krol <Krol.kevin@gmail.com>
2016-03-08 19:50:41 +0100

    Added enemy dead behaviour

---
+++ Assets/Scripts/Entity/Enemy/EnemyDeathBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyDeathBehaviour : MonoBehaviour
+{
+	private Entity entity;
+
+	protected void Awake()
+	{
+		entity = GetComponent<Entity>() ?? GetComponentInParent<Entity>();
+	}
+
+	protected void OnEnable()
+	{
+		entity.Events.AddListener<EntityDiedEvent>(OnDead);
+	}
+
+	protected void OnDisable()
+	{
+		entity.Events.RemoveListener<EntityDiedEvent>(OnDead);
+	}
+
+	private void OnDead(EntityDiedEvent evt)
+	{
+		Destroy(entity.gameObject);
+	}
+}
