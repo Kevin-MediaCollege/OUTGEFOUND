c9f069303d5a8efbf4e04a8b36bc5d3b762af840
Kevin Krol <Krol.kevin@gmail.com>
2016-03-15 20:28:54 +0100

    Added blood on screen

---
+++ Assets/Scripts/UI/HealthUI.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HealthUI : MonoBehaviour
+{
+	[SerializeField] private Image image;
+
+	private Entity player;
+	private EntityHealth health;
+
+	protected void LateUpdate()
+	{
+		if(player == null || health == null)
+		{
+			player = EntityUtils.GetEntityWithTag("Player");
+
+			if(player == null)
+			{
+				return;
+			}
+
+			health = player.GetHealth();
+
+			if(health == null)
+			{
+				return;
+			}
+		}
+
+		float current = health.CurrentHealth;
+		float max = health.StartingHealth;
+
+		float alpha = 1 - current / max;
+
+		Color color = image.material.color;
+		color.a = alpha * 0.65f;
+		image.material.color = color;
+	}
+}
