79373a90c27c4eba5e8664944595adfa88c4f843
ted-Thuis <meatkroket@hotmail.com>
2016-03-17 21:45:55 +0100

    Added bullet impact particles

---
+++ Assets/Scripts/Decal/Particles/BulletImpact.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using ParticlePlayground;
+
+public class BulletImpact : MonoBehaviour 
+{
+	[SerializeField]private PlaygroundParticlesC dust;
+	[SerializeField]private PlaygroundParticlesC pieces;
+
+	public void init()
+	{
+		dust.Emit(true);
+		pieces.Emit(true);
+	}
+}
