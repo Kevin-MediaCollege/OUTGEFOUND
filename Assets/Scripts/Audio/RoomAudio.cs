269af11bccff922ffca3e1acc8cda541cae99cb0
Kevin Krol <Krol.kevin@gmail.com>
2016-03-17 14:21:57 +0100

    Added ambient audio to level 1

---
+++ Assets/Scripts/Audio/RoomAudio.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RoomAudio : MonoBehaviour
+{
+	[SerializeField] private new AudioAsset audio;
+
+	[SerializeField] private float min;
+	[SerializeField] private float max;
+
+	protected void Awake()
+	{
+		AudioChannel audioChannel = Dependency.Get<AudioManager>().PlayAt(audio, transform.position, true);
+		audioChannel.IsClaimed = true;
+		audioChannel.AudioSource.minDistance = min;
+		audioChannel.AudioSource.maxDistance = max;
+	}
+
+	protected void OnDrawGizmosSelected()
+	{
+		Gizmos.color = Color.blue;
+		Gizmos.DrawWireSphere(transform.position, min);
+		Gizmos.DrawWireSphere(transform.position, max);
+	}
+}
