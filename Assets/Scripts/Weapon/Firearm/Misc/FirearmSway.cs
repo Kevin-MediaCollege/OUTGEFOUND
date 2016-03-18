94b260927e15e8152c47b2e6d5af5d36433f678b
Ted-Laptop-New <meatkroket@hotmail.com>
2016-03-18 19:41:37 +0100

    Working on gun sway

--- Assets/Scripts/Weapon/Firearm/Misc/FirearmSway.cs
+++ Assets/Scripts/Weapon/Firearm/Misc/FirearmSway.cs
@@ -3,12 +3,13 @@ using System.Collections;
 
 public class FirearmSway : MonoBehaviour
 {
+	/*
 	private Entity player;
 
 	private Transform weapon;
 	private Transform eyes;
 
-	private Vector2 start;
+	private Vector3 delay = new Vector3(0f, 0f, 0f);
 	private Vector2 target;
 	private Vector2 previous;
 
@@ -36,14 +37,16 @@ public class FirearmSway : MonoBehaviour
 			weapon = player.GetComponentInChildren<Weapon>().transform.parent;
 			eyes = player.transform.Find("Eyes");
 
-			start = new Vector2(eyes.rotation.eulerAngles.x, player.transform.rotation.eulerAngles.y);
+			delay = new Vector3(0f, 0f, 0f);
+			previous = new Vector2(eyes.rotation.eulerAngles.x, player.transform.rotation.eulerAngles.y);
+			target = previous;
 		}
 
+		previous = target;
 		float targetX = eyes.rotation.eulerAngles.x;
 		float targetY = player.transform.rotation.eulerAngles.y;
-
-		target = new Vector2(targetX - previous.x, targetY - previous.y);
-		previous = new Vector2(targetX, targetY);
+		target = new Vector2(targetX, targetY);
+		delay = Vector3.Slerp (delay, new Vector3 (previous.x - target.x, previous.y - target.y, 0f), 0.05f);
 	}
 
 	private IEnumerator	Lerp()
@@ -52,13 +55,17 @@ public class FirearmSway : MonoBehaviour
 		{
 			if(weapon != null)
 			{
-				target = Vector3.Slerp(target, start, Time.time * 0.1f);
+				//Quaternion newRotation = Quaternion.Euler(delay);
+				//weapon.localRotation.eulerAngles = delay;//Quaternion.Slerp(weapon.localRotation, newRotation, (Time.time * 0.1f));
 
-				Quaternion newRotation = Quaternion.Euler(target);
-				weapon.localRotation = Quaternion.Slerp(weapon.localRotation, newRotation, (Time.time * 0.1f));
+				Quaternion rot = weapon.localRotation;
+				Debug.Log (delay.x + "");
+				rot.eulerAngles = new Vector3(rot.eulerAngles.x, rot.eulerAngles.y, rot.eulerAngles.z);
+				weapon.localRotation = rot;
 			}
 
 			yield return null;
 		}
 	}
+	*/
 }
