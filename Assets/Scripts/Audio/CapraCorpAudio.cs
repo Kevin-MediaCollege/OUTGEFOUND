366b217ab650b3207ea3ddb88b1e272dc01cd35c
Kevin Krol <Krol.kevin@gmail.com>
2016-03-18 13:52:48 +0100

    Added Capra Corp intro sounds

---
+++ Assets/Scripts/Audio/CapraCorpAudio.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CapraCorpAudio : MonoBehaviour
+{
+	[SerializeField] private new AudioAssetGroup audio;
+	[SerializeField] private float interval;
+
+	private AudioManager audioManager;
+
+	protected void Awake()
+	{
+		audioManager = Dependency.Get<AudioManager>();
+	}
+
+	protected void OnEnable()
+	{
+		StartCoroutine("Play");
+	}
+
+	protected void OnDisable()
+	{
+		StopCoroutine("Play");
+	}
+
+	private IEnumerator Play()
+	{
+		while(true)
+		{
+			audioManager.PlayRandom(audio);
+
+			yield return new WaitForSeconds(interval);
+		}
+	}
+}
