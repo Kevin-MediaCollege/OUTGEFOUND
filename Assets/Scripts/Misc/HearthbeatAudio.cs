94cae18ee53ced79e52bc9501012f53f438f6477
Kevin Krol <Krol.kevin@gmail.com>
2016-03-18 11:11:51 +0100

    Added hearthbeat audio

---
+++ Assets/Scripts/Misc/HearthbeatAudio.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HearthbeatAudio : MonoBehaviour
+{
+	[SerializeField] private new AudioAsset audio;
+
+	private AudioManager audioManager;
+	private AudioChannel audioChannel;
+
+	private Entity player;
+	private EntityHealth health;
+
+	protected void Awake()
+	{
+		audioManager = Dependency.Get<AudioManager>();
+	}
+
+	protected void Update()
+	{
+		if(player == null || health == null)
+		{
+			player = EntityUtils.GetEntityWithTag("Player");
+
+			if(player == null)
+			{
+				return;
+			}
+
+			health = player.GetHealth();
+		}
+
+		if(health.CurrentHealth <= (health.StartingHealth / 4))
+		{
+			if(audioChannel == null || !audioChannel.IsPlaying)
+			{
+				audioChannel = audioManager.Play(audio);
+			}
+		}
+	}
+}
