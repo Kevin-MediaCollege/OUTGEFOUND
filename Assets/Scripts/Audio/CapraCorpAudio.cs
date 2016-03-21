47632a1fcbca6cbb0a37d306671b11714ed5be70
Kevin Krol <Krol.kevin@gmail.com>
2016-03-21 08:56:32 +0100

    Improved capra corp audio

--- Assets/Scripts/Audio/CapraCorpAudio.cs
+++ Assets/Scripts/Audio/CapraCorpAudio.cs
@@ -4,7 +4,12 @@ using System.Collections;
 public class CapraCorpAudio : MonoBehaviour
 {
 	[SerializeField] private new AudioAssetGroup audio;
+	[SerializeField] private Transform[] speakers;
+
 	[SerializeField] private float interval;
+
+	[SerializeField] private float min;
+	[SerializeField] private float max;
 
 	private AudioManager audioManager;
 
@@ -23,11 +28,31 @@ public class CapraCorpAudio : MonoBehaviour
 		StopCoroutine("Play");
 	}
 
+	protected void OnDrawGizmosSelected()
+	{
+		Gizmos.color = Color.blue;
+
+		foreach(Transform speaker in speakers)
+		{
+			Gizmos.DrawWireSphere(speaker.position, min);
+			Gizmos.DrawWireSphere(speaker.position, max);
+		}
+	}
+
 	private IEnumerator Play()
 	{
+		yield return new WaitForSeconds(2f);
+
 		while(true)
 		{
-			audioManager.PlayRandom(audio);
+			AudioAsset target = audio.GetRandom();
+
+			foreach(Transform speaker in speakers)
+			{
+				AudioChannel audioChannel = audioManager.PlayAt(target, speaker.position, true);
+				audioChannel.AudioSource.minDistance = min;
+				audioChannel.AudioSource.maxDistance = max;
+			}
 
 			yield return new WaitForSeconds(interval);
 		}
