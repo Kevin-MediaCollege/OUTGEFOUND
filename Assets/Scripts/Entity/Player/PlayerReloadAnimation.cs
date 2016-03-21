945e779976b6be6d93adbac22fba559084783fa7
Kevin Krol <Krol.kevin@gmail.com>
2016-03-21 13:46:33 +0100

    Added reload animation

---
+++ Assets/Scripts/Entity/Player/PlayerReloadAnimation.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerReloadAnimation : MonoBehaviour
+{
+	[SerializeField] private Animator animator;
+
+	private Entity entity;
+
+	protected void Awake()
+	{
+		entity = GetComponentInParent<Entity>();
+	}
+
+	protected void OnEnable()
+	{
+		entity.Events.AddListener<ReloadEvent>(OnReloadEvent);
+		entity.Events.AddListener<ReloadDoneEvent>(OnReloadDoneEvent);
+	}
+
+	protected void OnDisable()
+	{
+		entity.Events.AddListener<ReloadEvent>(OnReloadEvent);
+		entity.Events.AddListener<ReloadDoneEvent>(OnReloadDoneEvent);
+	}
+
+	private void OnReloadEvent(ReloadEvent evt)
+	{
+		animator.SetBool("Reloading", true);
+	}
+
+	private void OnReloadDoneEvent(ReloadDoneEvent evt)
+	{
+		animator.SetBool("Reloading", false);
+	}
+}
