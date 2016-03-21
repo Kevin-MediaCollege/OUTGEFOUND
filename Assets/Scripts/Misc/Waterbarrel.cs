0cf0d0f596c67d8c29c4a40f05e30308483beac1
Kevin Krol <Krol.kevin@gmail.com>
2016-03-21 10:45:15 +0100

    Added physics to waterbarrels

---
+++ Assets/Scripts/Misc/Waterbarrel.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Waterbarrel : MonoBehaviour
+{
+	[SerializeField] private Entity entity;
+	[SerializeField] private new Rigidbody rigidbody;
+
+	protected void OnEnable()
+	{
+		entity.Events.AddListener<WeaponDamageEvent>(OnWeaponDamageEvent);
+	}
+
+	protected void OnDisable()
+	{
+		entity.Events.RemoveListener<WeaponDamageEvent>(OnWeaponDamageEvent);
+	}
+
+	private void OnWeaponDamageEvent(WeaponDamageEvent evt)
+	{
+		rigidbody.AddForce(-evt.Hit.normal * 7, ForceMode.Impulse);
+	}
+}
