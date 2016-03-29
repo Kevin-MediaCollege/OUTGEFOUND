fb5448ff607154bd426b042376a6b74b8b2d7971
Kevin Krol <Krol.kevin@gmail.com>
2016-03-29 12:16:13 +0200

    Cleaned up FlyCam

--- Assets/Scripts/Camera/FlyCam.cs
+++ Assets/Scripts/Camera/FlyCam.cs
@@ -1,87 +1,89 @@
 using UnityEngine;
-using System.Collections;
 
-public class FlyCam : MonoBehaviour {
-	
-	
-	public float speed = 50.0f;		// max speed of camera
-	public float sensitivity = 0.25f; 		// keep it from 0..1
-	public bool inverted = false;
-	
-	
-	private Vector3 lastMouse = new Vector3(255, 255, 255);
-	
-	
-	// smoothing
-	public bool smooth = true;
-	public float acceleration = 0.1f;
-	private float actSpeed = 0.0f;			// keep it from 0 to 1
-	private Vector3 lastDir = new Vector3();
-	
-	
-	// Use this for initialization
-	void Start () {
-	
-	}
+public class FlyCam : MonoBehaviour
+{
+	[SerializeField] private float speed = 50.0f;
+	[SerializeField] private float sensitivity = 0.25f;
 
-    float rotationX;
-    float rotationY;
+	[SerializeField] private bool smooth = true;
+	[SerializeField] private float acceleration = 0.1f;
+
+	private Vector3 lastDir;
+
+	private float actSpeed;
+    private float rotationX;
+    private float rotationY;
 	
-	// Update is called once per frame
-	void Update () {
+	protected void Update () {
         rotationX += Input.GetAxis("Mouse X") * sensitivity;
         rotationY += Input.GetAxis("Mouse Y") * sensitivity;
 
-       transform.rotation = Quaternion.AngleAxis(rotationX, Vector3.up);
+		transform.rotation = Quaternion.AngleAxis(rotationX, Vector3.up);
         transform.rotation *= Quaternion.AngleAxis(rotationY, Vector3.left);
- 
-		
-		
-		
-		
-		
-		// Movement of the camera
-		
-		Vector3 dir = new Vector3();			// create (0,0,0)
-		
-		if (Input.GetKey(KeyCode.W)) dir.z += 1.0f;
-		if (Input.GetKey(KeyCode.S)) dir.z -= 1.0f;
-		if (Input.GetKey(KeyCode.A)) dir.x -= 1.0f;
-		if (Input.GetKey(KeyCode.D)) dir.x += 1.0f;
-        if (Input.GetKey(KeyCode.LeftControl)) dir.y -= 1.0f;
-        if (Input.GetKey(KeyCode.LeftShift)) dir.y += 1.0f;
-        dir.Normalize();
-		
-		
-		if (dir != Vector3.zero) {
-			// some movement 
-			if (actSpeed < 1)
-				actSpeed += acceleration * Time.deltaTime * 40;
-			else 
-				actSpeed = 1.0f;
-			
-			lastDir = dir;
-		} else {
-			// should stop
-			if (actSpeed > 0)
-				actSpeed -= acceleration * Time.deltaTime * 20;
-			else 
-				actSpeed = 0.0f;
+
+		Vector3 dir = new Vector3();
+
+		if(Input.GetKey(KeyCode.W))
+		{
+			dir.z += 1.0f;
 		}
-		
-		
-		
-		
-		if (smooth) 
-			transform.Translate( lastDir * actSpeed * speed * Time.deltaTime );
-		
-		else 
-			transform.Translate ( dir * speed * Time.deltaTime );
-		
-		
-	}
-	
-	void OnGUI() {
-		GUILayout.Box ("actSpeed: " + actSpeed.ToString());
+		else if(Input.GetKey(KeyCode.S))
+		{
+			dir.z -= 1.0f;
+		}
+
+		if(Input.GetKey(KeyCode.A))
+		{
+			dir.x -= 1.0f;
+		}
+		else if(Input.GetKey(KeyCode.D))
+		{
+			dir.x += 1.0f;
+		}
+
+		if(Input.GetKey(KeyCode.LeftControl))
+		{
+			dir.y -= 1.0f;
+		}
+		else if(Input.GetKey(KeyCode.LeftShift))
+		{
+			dir.y += 1.0f;
+		}
+
+        dir.Normalize();
+
+		if(dir != Vector3.zero)
+		{
+			if(actSpeed < 1)
+			{
+				actSpeed += acceleration * Time.deltaTime * 40;
+			}
+			else
+			{
+				actSpeed = 1.0f;
+			}
+
+			lastDir = dir;
+		}
+		else
+		{
+			if(actSpeed > 0)
+			{
+				actSpeed -= acceleration * Time.deltaTime * 20;
+			}
+			else
+			{
+				actSpeed = 0.0f;
+			}
+		}
+
+		if(smooth)
+		{
+			transform.Translate(lastDir * actSpeed * speed * Time.deltaTime);
+		}
+		else
+		{
+			transform.Translate(dir * speed * Time.deltaTime);
+		}
 	}
 }
